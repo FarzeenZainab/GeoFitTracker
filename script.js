383e79320a5189de6a7ed72b6044f46d6b5a2e2f
Farzeen Zainab <farzeenzaidi51@gmail.com>
2024-05-21 22:41:09 +0500

    show workout in the list

--- script.js
+++ script.js
@@ -4,12 +4,16 @@
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 
 const form = document.querySelector('.form');
-const containerWorkouts = document.querySelector('.workouts');
 const inputType = document.querySelector('.form__input--type');
 const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+const workouts = [];
+const containerWorkouts = document.querySelector('.workouts');
+
+let map, coords;
 
 //  get current position takes in two callbacks, one is called when the location
 // is successfully retrieved and the second one when there is an error or the user denies to
@@ -18,40 +22,120 @@ navigator?.geolocation?.getCurrentPosition(
   position => {
     // get user's position
     const { latitude, longitude } = position.coords;
-    const coords = [latitude, longitude];
+    const currentLocation = [latitude, longitude];
 
     // create map
     const key = 'FakHOIPI0AUyYkhvQldq';
-    const map = L.map('map').setView(coords, 16);
+    map = L.map('map').setView(currentLocation, 16);
 
     L.maptilerLayer({
       apiKey: key,
-      style: 'dataviz', //optional
+      // style: 'dataviz', //optional
     }).addTo(map);
 
-    // drop a new pin on click
-    map.addEventListener('click', e => {
-      if (e.type === 'click') {
-        const { lat, lng } = e.latlng;
-        const coordinates = [lat, lng];
+    // handling click on map
+    map.addEventListener('click', mapEvent => {
+      form.classList.remove('hidden');
+      inputDistance.focus();
 
-        L.marker(coordinates)
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('workout')
-          .openPopup();
-      }
+      coords = mapEvent.latlng;
     });
   },
   () => {
     alert('Could not get your location');
   }
 );
+
+form.addEventListener('submit', e => {
+  e.preventDefault();
+
+  // today's date
+  const today = new Date().toLocaleDateString('en-US', {
+    month: 'long',
+    day: 'numeric',
+  });
+
+  // get all form values
+  const formData = new FormData(form);
+  const formValues = {
+    type: formData.get('type'),
+    distance: formData.get('distance'),
+    duration: formData.get('duration'),
+  };
+
+  if (formValues.type === 'running') {
+    formValues.cadence = formData.get('cadence');
+
+    containerWorkouts.insertAdjacentHTML(
+      'beforeend',
+      `  <li class="workout workout--running"">
+          <h2 class="workout__title">Running on April 14</h2>
+          <div class="workout__details">
+            <span class="workout__icon">🏃‍♂️</span>
+            <span class="workout__value">${formValues.distance}</span>
+            <span class="workout__unit">km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⏱</span>
+            <span class="workout__value">${formValues.duration}</span>
+            <span class="workout__unit">min</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">⚡️</span>
+            <span class="workout__value">${Math.ceil(
+              formValues.cadence / formValues.duration
+            )}</span>
+            <span class="workout__unit">min/km</span>
+          </div>
+          <div class="workout__details">
+            <span class="workout__icon">🦶🏼</span>
+            <span class="workout__value">${formValues.cadence}</span>
+            <span class="workout__unit">spm</span>
+          </div>
+        </li>
+`
+    );
+  }
+
+  if (formValues.type === 'cycling') {
+    formValues.elevation = formData.get('elevation');
+  }
+
+  workouts.push({
+    formValues,
+  });
+
+  // display the marker
+  const coordinates = [coords.lat, coords.lng];
+  L.marker(coordinates)
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent(
+      `<span style="text-transform: capitalize;">${formValues.type}</span> on ${today}`
+    )
+    .openPopup();
+
+  // clear the input values
+  inputCadence.value =
+    inputDistance.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+
+  // hide form
+  form.classList.add('hidden');
+});
+
+// on type change
+inputType.addEventListener('change', e => {
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+});
